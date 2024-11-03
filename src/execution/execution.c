d55ce2ee844ac0167a290b958b9ed25082aa79c1
sargsyandavid7 <sargsyand648@gmail.com>
2024-11-03 18:44:02 +0400

    to do : tree free, exec , norminette

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -6,11 +6,14 @@
 /*   By: dasargsy <dasargsy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 19:47:54 by dasargsy          #+#    #+#             */
-/*   Updated: 2024/11/03 18:38:55 by dasargsy         ###   ########.fr       */
+/*   Updated: 2024/11/03 18:43:33 by dasargsy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
 extern int g_status;
+
+
+// NEED TO WRITE EXECUTION FOR COMMANDS AND OPERATORS
 
---
+++ src/parser/free_tree.c
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_tree.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dasargsy <dasargsy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/03 18:40:31 by dasargsy          #+#    #+#             */
+/*   Updated: 2024/11/03 18:43:20 by dasargsy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+// NEED TO WRITE FREE FOR COMMANDS AND OPERATORS
+// NEED TO CONNECT ALL FREE FUNCTIONS
